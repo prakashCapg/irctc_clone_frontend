ca14ec443899aba829192f53a6acbac6cf15727c
B Bharath Kumar Reddy <b-bharath-kumar.reddy@capgemini.com>
2026-02-17 11:11:35 +0530

    testing setup of train-search page

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+  },
+};
